348d1425614211be930ea44211c9a9f1488e7d58
Joabesv <joabevarjao123@gmail.com>
2023-01-06 15:16:23 -0300

    fix(refactor): cleaning things

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,31 +1,12 @@
-import { useState } from 'react'
 import './global.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <div className="App">
+    <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-        </a>
+        Hello React
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   )
 }
 
-export default App
