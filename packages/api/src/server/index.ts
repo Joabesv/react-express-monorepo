ed812ccabe2c149c45da51681fc90a63dd26da02
Joabesv <joabevarjao123@gmail.com>
2023-01-07 23:03:01 -0300

    feat(cors): implement cors middleware

--- packages/api/src/server/index.ts
+++ packages/api/src/server/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { connection } from '../database/connection';
 import { contestRoutes } from '../routes';
 
 export const buildServer = async () => {
   const server = express();
   await connection();
+  server.use(cors());
   server.use('/api', contestRoutes);
   return server;
 };
